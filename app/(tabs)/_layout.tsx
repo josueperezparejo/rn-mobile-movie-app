7af9e75a6dedf6251e87d39246c8ab9411c23817
Josue Perez <josue_2707@hotmail.com>
2025-07-27 15:01:40 -0500

    Enhance tab layout with custom icons and styles. Added TabBarIcon component for better visual representation of tabs, including Home, Profile, Saved, and Search screens. Updated tab bar styles for improved aesthetics.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,15 +1,70 @@
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
 import { Tabs } from "expo-router";
 import React from "react";
-import { StyleSheet } from "react-native";
+import { Image, ImageBackground, StyleSheet, Text, View } from "react-native";
 
+const TabBarIcon = ({
+  focused,
+  icon,
+  label,
+}: {
+  focused: boolean;
+  icon: any;
+  label: string;
+}) => {
+  if (focused) {
+    return (
+      <ImageBackground
+        className="mt-4 flex min-h-16 w-full min-w-[112px] flex-1 flex-row items-center justify-center overflow-hidden rounded-full"
+        source={images.highlight}
+      >
+        <Image source={icon} tintColor={"#151312"} className="size-5" />
+        <Text className="ml-2 text-base font-semibold text-secondary">
+          {label}
+        </Text>
+      </ImageBackground>
+    );
+  }
+
+  return (
+    <View className="mt-4 size-full items-center justify-center rounded-full">
+      <Image source={icon} tintColor={"#A8B25DB"} className="size-5" />
+    </View>
+  );
+};
 const Layout = () => {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarItemStyle: {
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        },
+        tabBarStyle: {
+          backgroundColor: "#0F0D23",
+          borderRadius: 50,
+          marginHorizontal: 20,
+          marginBottom: 34,
+          height: 52,
+          position: "absolute",
+          overflow: "hidden",
+          borderWidth: 1,
+          borderColor: "#0F0D23",
+        },
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
           title: "Home",
           headerShown: false,
+          tabBarIcon: ({ focused, color, size }) => (
+            <TabBarIcon focused={focused} icon={icons.logo} label="Home" />
+          ),
         }}
       />
 
@@ -18,6 +73,9 @@ const Layout = () => {
         options={{
           title: "Profile",
           headerShown: false,
+          tabBarIcon: ({ focused, color, size }) => (
+            <TabBarIcon focused={focused} icon={icons.person} label="Profile" />
+          ),
         }}
       />
 
@@ -26,6 +84,9 @@ const Layout = () => {
         options={{
           title: "Saved",
           headerShown: false,
+          tabBarIcon: ({ focused, color, size }) => (
+            <TabBarIcon focused={focused} icon={icons.save} label="Saved" />
+          ),
         }}
       />
 
@@ -34,6 +95,9 @@ const Layout = () => {
         options={{
           title: "Search",
           headerShown: false,
+          tabBarIcon: ({ focused, color, size }) => (
+            <TabBarIcon focused={focused} icon={icons.search} label="Search" />
+          ),
         }}
       />
     </Tabs>
