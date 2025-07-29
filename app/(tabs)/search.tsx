30a9611ab99ab16bf972fc7f064e70ec0cf0e4cc
Josue Perez <josue_2707@hotmail.com>
2025-07-28 20:57:30 -0500

    Implement search functionality in the Search tab by integrating MovieCard and SearchBar components. Utilize FlatList for rendering movie results with improved layout and responsiveness, including a background image and enhanced header. Fetch movies based on a search query with loading and error handling states.

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,10 +1,61 @@
+import MovieCard from "@/components/MovieCard";
+import SearchBar from "@/components/SearchBar";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { getMovies } from "@/services/api";
+import { useFetch } from "@/services/useFetch";
+import { useRouter } from "expo-router";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, Image, StyleSheet, View } from "react-native";
 
 const Search = () => {
+  const router = useRouter();
+
+  const {
+    data: movies,
+    error,
+    loading,
+    refetch,
+    reset,
+  } = useFetch(() => getMovies({ query: "iron man" }), true);
+
   return (
-    <View>
-      <Text>Search</Text>
+    <View className="flex-1 bg-primary">
+      <Image
+        source={images.bg}
+        className="absolute z-0 w-full flex-1"
+        resizeMode="cover"
+      />
+
+      <FlatList
+        className="flex-1"
+        data={movies?.results}
+        renderItem={({ item }) => <MovieCard {...item} />}
+        keyExtractor={(item) => item.id}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          paddingBottom: 80,
+          width: "100%",
+        }}
+        numColumns={3}
+        columnWrapperStyle={{ justifyContent: "space-between" }}
+        ListHeaderComponent={() => (
+          <>
+            <View className="mt-20 w-full flex-row items-center justify-center">
+              <Image source={icons.logo} className="size-5 h-10 w-12" />
+            </View>
+
+            <View className="my-5 w-full flex-row items-center justify-center p-4">
+              <SearchBar
+                placeholder="Search for a movie"
+                onPress={() => {
+                  router.push("/search");
+                }}
+              />
+            </View>
+          </>
+        )}
+      />
     </View>
   );
 };
